f75765f5575d85eca8582f8e2d7d003bb615602b
Design <dmercadoff@gmail.com>
2021-05-25 19:16:23 -0400

    tra

--- templates/9843b5429480096f13d1b3baf69e5ef03415b019_0.file.sidebar.tpl.php
+++ templates/9843b5429480096f13d1b3baf69e5ef03415b019_0.file.sidebar.tpl.php
@@ -122,7 +122,7 @@ $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
 							<?php echo lang('stories_from_people');?>
 
 							<span class="create-new-post btn btn-primary" data-type="story">
-					      		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg> <?php echo lang('add_story');?>
+					      		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg> Añadir Historia
 
 					      	</span>
 						</span>
