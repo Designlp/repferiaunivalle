475b591aba59a3c4e1e379e57e46e121cbc40031
Design <dmercadoff@gmail.com>
2021-05-25 00:44:31 -0400

    subiendo

---
+++ sys/config.php
@@ -0,0 +1,16 @@
+<?php
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "u532654912_feriaunivalle";
+// MySQL Database Password
+$sql_db_pass = "gtaV19921963";
+// MySQL Database Name
+$sql_db_name = "u532654912_feriaunivalle";
+
+// Site URL
+$site_url = "http://feriaunivalle.ml"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "purchase code"; // Your purchase code, don\'t give it to anyone. 
+$buyer = "buyer"; // Your buyer name from envato. 
+?>
