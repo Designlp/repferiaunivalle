aa6e415973cb9ad723a40a4d36178aa0632a9ac6
Design <dmercadoff@gmail.com>
2021-05-26 20:50:41 -0400

    traduciendo

--- sys/server/utils.php
+++ sys/server/utils.php
@@ -232,7 +232,7 @@ function time2str($ptime) {
         $d = $etime / $secs;
         if ($d >= 1) {
             $r = round($d);
-            return $r . ' ' . lang($str)) .($r > 1 ? lang('time_ago') :' ' . lang($a_plural[$str]);
+            return  ' ' . ($r > 1 ? lang('time_ago') : lang($str)) .' '. $r.' ' . lang($a_plural[$str]);
         }
     }
 }
